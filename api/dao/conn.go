43a47b0f4d7e41603eb3916f0ffaa8772c1d412c
xiexianhui <xiexianhuicn@outlook.com>
2022-02-08 16:07:56 +0800

    "fix bug"

--- api/dao/conn.go
+++ api/dao/conn.go
@@ -11,7 +11,7 @@ var (
 )
 
 func init() {
-	dbConn, err = sql.Open("mysql", "root:Casey151224!@tcp(127.0.0.1:3306)/video_server?charset=utf8")
+	dbConn, err = sql.Open("mysql", "Andy401927!@tcp(127.0.0.1:3306)/video_server?charset=utf8")
 	if err != nil {
 		panic(err)
 	}
