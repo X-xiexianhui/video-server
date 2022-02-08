1fdf800101718234a374064d3ffa8e7096e130e7
xiexianhui <xiexianhuicn@outlook.com>
2022-02-08 15:40:19 +0800

    "build script"

--- buildprod.sh
+++ buildprod.sh
@@ -11,7 +11,7 @@ env GOOS=linux GOARCH=amd64 go build -o ../bin/api
 cd ~/go/src/video_server/scheduler
 env GOOS=linux GOARCH=amd64 go build -o ../bin/scheduler
 
-cd ~/go/src/video_server/streamserver
+cd ~/go/src/video_server/streamServer
 env GOOS=linux GOARCH=amd64 go build -o ../bin/streamserver
 
 cd ~/go/src/video_server/web
