d0eaa2e24665aba8ead0b544006c1c3387933f82
xiexianhui <xiexianhuicn@outlook.com>
2022-02-08 13:28:23 +0800

    "build script"

--- build.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# Build web UI
-cd ~/work/src/video_server/web
-go install
-cp ~/work/bin/web ~/work/bin/video_server_web_ui/web
-cp -R ~/work/src/video_server/templates ~/work/bin/video_server_web_ui/
