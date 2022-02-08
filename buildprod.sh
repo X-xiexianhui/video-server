dcc7cfe774f571f46d35b3a5ac0aa8ae33ff9816
xiexianhui <xiexianhuicn@outlook.com>
2022-02-08 13:39:51 +0800

    "build script"

--- buildprod.sh
+++ buildprod.sh
@@ -5,14 +5,14 @@ cp -R ./templates ./bin/
 mkdir ./bin/videos
 # Build web and other services
 
-cd ./work/src/video_server/api
+cd ~/work/src/video_server/api
 env GOOS=linux GOARCH=amd64 go build -o ../bin/api
 
-cd ./work/src/video_server/scheduler
+cd ~/work/src/video_server/scheduler
 env GOOS=linux GOARCH=amd64 go build -o ../bin/scheduler
 
-cd ./work/src/video_server/streamserver
+cd ~/work/src/video_server/streamserver
 env GOOS=linux GOARCH=amd64 go build -o ../bin/streamserver
 
-cd ./work/src/video_server/web
+cd ~/work/src/video_server/web
 env GOOS=linux GOARCH=amd64 go build -o ../bin/web
