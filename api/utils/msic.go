a9362475e9b00fb265d744da479b1a7fdd7de17d
xiexianhui <xiexianhuicn@outlook.com>
2022-02-09 10:58:01 +0800

    "fix bug"

--- api/utils/msic.go
+++ api/utils/msic.go
@@ -18,7 +18,7 @@ func NewUUID() (string, error) {
 	}
 	uuid[8] = uuid[0]&^0xc0 | 0x80
 	uuid[6] = uuid[6]&^0xf0 | 0x40
-	return fmt.Sprintf("%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8]), nil
+	return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:]), nil
 }
 
 func GetCurrentTimestampSec() int {
